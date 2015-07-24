f2ab1f9cb9e3d1e93cdaae3a39e182be8be55fd6
Lisa Vuong <hello.lisavuong@gmail.com>
2015-07-24 10:17:47 -0500

    All da files

---
+++ card.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+  var i = 1;
+  var cardLength = $('li.card').size();
+  $('.button').click(function(){
+   /* for (n = 0; n < $('li.card').size(); n++){*/
+    $('.card-'+i).addClass('removed');
+    $('.card-'+ (i+1)).addClass('front');
+    $('.card-'+ (i+2)).addClass('back-2');
+    $('.card-'+ (i+3)).addClass('back-3');
+    $('.card-'+ (i+4)).addClass('back-4');
+    $('.card-'+ (i+5)).addClass('back-5');
+    
+     $('.button-set-'+i).addClass('removed');
+    $('.button-set-'+ (i+1)).addClass('active');
+    $('.card-'+ (i+2)).addClass('back-2');
+    
+    i++;
+    
+    /*}*/
+  })
+})
