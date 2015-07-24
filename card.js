c9a7d317e6fff7cfe7b34a9e10dfba432c50a90c
Lisa Vuong <hello.lisavuong@gmail.com>
2015-07-24 13:00:22 -0500

    Buttons Light Up and Things Work on Chrome Now :B
    Added Chrome Webkits
    Buttons light up on click, but not on tap
    Changed some font things

--- card.js
+++ card.js
@@ -14,8 +14,30 @@ $(document).ready(function(){
     $('.button-set-'+ (i+1)).addClass('active');
     $('.card-'+ (i+2)).addClass('back-2');
     
+    
+    
     i++;
     
     /*}*/
   })
+  $('.yellow-bg').click(function(){
+      $(this).addClass("yellow-click")
+    })
+    
+    $('.green-bg').click(function(){
+      $(this).addClass("green-click")
+    })
+
+     $('.purple-bg').click(function(){
+      $(this).addClass("purple-click")
+    })
+
+      $('.blue-bg').click(function(){
+      $(this).addClass("blue-click")
+    })
+
+       $('.red-bg').click(function(){
+      $(this).addClass("red-click")
+    })
+
 })
